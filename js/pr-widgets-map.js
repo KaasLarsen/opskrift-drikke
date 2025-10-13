5ae3c60dc27ae25a3c78628ced36be1695bc7cc9
KaasLarsen <vallelarsen@yahoo.dk>
2025-10-13 12:09:40 +0200

    Update pr-widgets-map.js

--- js/pr-widgets-map.js
+++ js/pr-widgets-map.js
@@ -6,26 +6,34 @@ export const fold = (s='') => s.toLowerCase()
 
 // prioriteret liste af widget-keys pr. kategori/tag
 export const CATEGORY_TO_WIDGET_KEYS = {
+  // Smoothies
   'smoothie': ['blender-ninja-1','blender-nutribullet-1','blender-wilfa-1'],
 
+  // Shots
   'shots': ['slowjuicer-philips-1','citrus-braun-1','citrus-solis-1'],
   'sunde shots': ['slowjuicer-philips-1','citrus-braun-1','citrus-solis-1'],
 
+  // Kaffe
   'kaffe': ['kaffemaskine-delonghi-1','kaffekvaern-timemore-1','maelkeskummer-severin-1'],
 
+  // Te & varme drikke
   'te': ['kedel-hario-12','kedel-hario-10','kedel-hario-temp'],
   'te & varme drikke': ['kedel-hario-12','kedel-hario-10','kedel-hario-temp'],
 
+  // Mocktails / uden alkohol
   'mocktails': ['sodastream-duo-1','sodastream-duo-2','sodastream-duo-3'],
   'uden alkohol': ['sodastream-duo-1','sodastream-duo-2','sodastream-duo-3'],
 
+  // Cocktails / med alkohol
   'cocktails': ['boston-zone-1','boston-pro-1','boston-barcraft-1'],
   'med alkohol': ['boston-zone-1','boston-pro-1','boston-barcraft-1'],
 
-  'juice': ['juice-philips-1','juice-braun-1','juice-solis-1'],
-  'saft':  ['juice-philips-1','juice-braun-1','juice-solis-1'],
+  // Juice & saft (rettet → slowjuicer/citrus)
+  'juice': ['slowjuicer-philips-1','citrus-braun-1','citrus-solis-1'],
+  'saft':  ['slowjuicer-philips-1','citrus-braun-1','citrus-solis-1'],
 
-  'gloegg': ['stanley-termoflaske-1','gryde-tefal-5l','gryde-dorre-5l'], // foldet form
+  // Gløgg
+  'gloegg': ['stanley-termoflaske-1','gryde-tefal-5l','gryde-dorre-5l'],
 };
 
 // vælg bedste widget for en recipe
